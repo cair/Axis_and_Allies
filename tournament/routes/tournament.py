22fd62fd595bc40f3f5101b12468ce2f9950f05c
Per-Arne Andersen <per@sysx.no>
2018-10-01 13:24:39 +0200

    Updated GUI of Tournament Log

--- tournament/routes/tournament.py
+++ tournament/routes/tournament.py
@@ -71,27 +71,26 @@ class TournamentHandler(tornado.web.RequestHandler):
 
         return bot_instance
 
-    def run_game(self, bot_1, bot_2):
+    def run_game(self, bot_1, bot_2, n_games=6, w=6, h=6):
         germany = Nation(name=str(bot_1.__module__), bot=bot_1)
         russia = Nation(name=str(bot_2.__module__), bot=bot_2)
-        results = {}
+        results = {
+            'bot_1': {'bot_name': str(bot_1.__module__), 'wins': 0, 'avg_rounds': 0} ,
+            'bot_2': {'bot_name': str(bot_2.__module__), 'wins': 0, 'avg_rounds': 0}
 
-        results['bot_1'] = {'bot_name': str(bot_1.__module__), 'wins': 0, 'avg_rounds': 0}
-        results['bot_2'] = {'bot_name': str(bot_2.__module__), 'wins': 0, 'avg_rounds': 0}
+        }
 
-        number_of_rounds = 10
-        for i in range(0, number_of_rounds):
-            game = Game(size=(6, 6), nations=[germany, russia])
+        for i in range(0, n_games):
+            game = Game(size=(w, h), nations=[germany, russia])
             while True:
                 game.bot()
                 is_there_a_winner, winner = game.is_there_a_winner()
                 if is_there_a_winner:
                     if winner == game.nations[0]:
                         results['bot_1']['wins'] +=1
-                        results['bot_1']['avg_rounds'] += game.turn/number_of_rounds
+                        results['bot_1']['avg_rounds'] += game.turn / n_games
                     elif winner == game.nations[1]:
                         results['bot_2']['wins'] += 1
-                        results['bot_2']['avg_rounds'] += game.turn / number_of_rounds
+                        results['bot_2']['avg_rounds'] += game.turn / n_games
                     break
-
         return results
